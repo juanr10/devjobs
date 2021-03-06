dbe3db48d8b24928cc89f0d599b659ee006ac2e4
juanr10 <argudojuan7@gmail.com>
2021-03-06 14:50:09 +0100

    lightbox2 for images installed & configured

--- resources/views/vacants/show.blade.php
+++ resources/views/vacants/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" integrity="sha512-ZKX+BvQihRJPA8CROKBhDNvoc2aDMOdAlcm7TUQY+35XYtrd3yh95QOOhsPDQY9QnKE0Wqag9y38OIgEvb88cA==" crossorigin="anonymous" />
+@endsection
+
 @section('content')
     <h1 class="text-2xl text-center mt-10">{{ $vacant->title }}</h1>
 
@@ -32,6 +36,14 @@
                         {{ $skill }}
                     </p>
                 @endforeach
+            </div>
+
+            <div class="flex flex-wrap justify-center">
+                <div class="w-6/12 sm:w-4/12 px-4">
+                    <a href="/storage/vacants/{{ $vacant->image }}" data-lightbox="image" data-title="{{ $vacant->title }}">
+                        <img src="/storage/vacants/{{ $vacant->image }}" alt="vacant-image" class="shadow-lg rounded max-w-full h-auto align-middle border-none">
+                    </a>
+                </div>
             </div>
 
             <div class="description mt-8 mb-5">
