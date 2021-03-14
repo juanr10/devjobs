5aebf935bf6c129ac07f73c3c5638539c2ad2ff2
juanr10 <argudojuan7@gmail.com>
2021-03-14 02:10:37 +0100

    DEV-4 Register & login styles completed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,70 +1,117 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container mx-auto max-w-screen-md">
-    <div class="flex flex-wrap justify-center">
-        <div class="md:w-1/2 order-2 md:order-1">
-            <div class="w-full max-w-sm">
-                <div class="flex flex-col break-words bg-white border-2 shadow-md mt-20">
-                    <div class="bg-gray-300 text-gray-700 uppercase text-center py-3 px-6 mb-0">
-                        {{ __('Register') }}
+@section('login&Register')
+<div class="w-full flex flex-wrap">
+    <div class="w-full md:w-1/2 flex flex-col">
+        <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
+            <p class="text-center text-3xl text-black font-bold">   <svg class="fill-current inline pb-1" width="45" height="45" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
+            </svg> ¿Es reclutador?</p>
+            <p class="text-center text-2xl text-black">Regístrese y empiece a publicar vacantes.</p>
+
+            <form class="flex flex-col pt-3 md:pt-8" method="POST" action="{{ route('register') }}" novalidate>
+                @csrf
+                <div class="flex flex-col pt-4">
+                    <div class="flex relative">
+                        <span
+                            class="inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" width="15" height="15">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                            </svg>
+                        </span>
+                        <input type="text" name="name" value="{{ old('name') }}"
+                            class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-600 shadow-sm text-base focus:ring focus:border-gray-500"
+                            placeholder="Nombre" />
                     </div>
-
-                    <form class="py-10 px-5" method="POST" action="{{ route('register') }}" novalidate>
-                        @csrf
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="name" class="block text-gray-700 text-sm mb-2">{{ __('Name') }}</label>
-                            <input id="name" type="text" class="p-3 bg-gray-200 rounded form-input w-full @error('name') border-red-500 border @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
-
-                            @error('name')
-                                <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="email" class="block text-gray-700 text-sm mb-2">{{ __('E-Mail Address') }}</label>
-                            <input id="email" type="email" class="p-3 bg-gray-200 rounded form-input w-full @error('email') border-red-500 border @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
-
-                            @error('email')
-                                <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="password" class="block text-gray-700 text-sm mb-2">{{ __('Password') }}</label>
-                            <input id="password" type="password" class="p-3 bg-gray-200 rounded form-input w-full @error('password') border-red-500 border @enderror" name="password" autocomplete="new-password">
-
-                            @error('password')
-                                <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="password-confirm" class="block text-gray-700 text-sm mb-2">{{ __('Confirm Password') }}</label>
-                            <input id="password-confirm" type="password" class="p-3 bg-gray-200 rounded form-input w-full" name="password_confirmation" autocomplete="new-password">
-                        </div>
-
-                        <div class="flex flex-wrap">
-                            <button type="submit" class="bg-teal-500 w-full hover:bg-teal-700 text-gray-100 p-3 focus:outline-none focus:shadow-outline font-semibold">
-                                {{ __('Register') }}
-                            </button>
-                        </div>
-                    </form>
+                    @error('name')
+                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm"
+                        role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
+
+                <div class="flex flex-col pt-4">
+                    <div class="flex relative">
+                        <span
+                            class="inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z">
+                                </path>
+                            </svg>
+                        </span>
+                        <input type="email" name="email" value="{{ old('email') }}"
+                            class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-600 shadow-sm text-base focus:ring focus:border-gray-500"
+                            placeholder="Email" />
+                    </div>
+                    @error('email')
+                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm"
+                        role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col pt-4">
+                    <div class="flex relative">
+                        <span
+                            class="inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z">
+                                </path>
+                            </svg>
+                        </span>
+                        <input type="password" name="password" value="{{ old('password') }}"
+                            class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-600 shadow-sm text-base focus:ring focus:border-gray-500"
+                            placeholder="Contraseña" />
+                    </div>
+                    @error('password')
+                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm"
+                        role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col pt-4">
+                    <div class="flex relative">
+                        <span
+                            class="inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z" />
+                            </svg>
+                        </span>
+                        <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}"
+                            class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-600 shadow-sm text-base focus:ring focus:border-gray-500"
+                            placeholder="Repetir contraseña" />
+                    </div>
+                </div>
+
+                <input type="submit" value="Registrar"
+                    class="w-full mt-6 px-4 py-2 text-base font-semibold text-center text-white transform transition hover:scale-105 duration-300 ease-in-out bg-teal-500 shadow-md hover:bg-teal-600 focus:outline-none focus:ring-2">
+            </form>
+            <div class="text-center pt-12 pb-12">
+                <p>
+                    ¿Ya tiene una cuenta?
+                    <a href="{{ route('login') }}" class="font-semibold underline">
+                        Inicie sesión aquí.
+                    </a>
+                </p>
             </div>
         </div>
+    </div>
 
-        <div class="md:w-1/2 order-1 md:order-2 text-center flex flex-col justify-center px-10 mt-10">
-            <h1 class="text-teal-500 text-2xl">¿Eres Reclutador?</h1>
-            <p class="text-xl mt-5 leading-7">Crea una cuenta y comienza a publicar vacantes.</p>
-        </div>
+    <div class="w-1/2 shadow-2xl">
+        <img class="object-cover w-full h-screen hidden md:block" src="{{ asset('img/work.jpg') }}" />
     </div>
 </div>
+
 @endsection
