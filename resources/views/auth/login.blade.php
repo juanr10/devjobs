82f9a3b6260ea5ddee672c00422ba65bf42911df
juanr10 <argudojuan7@gmail.com>
2021-03-14 01:06:36 +0100

    login styles updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,69 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container mx-auto">
-    <div class="flex flex-wrap justify-center">
-        <div class="w-full max-w-sm">
-            <div class="flex flex-col break-words bg-white border-2 shadow-md mt-20">
-                <div class="bg-gray-300 text-gray-700 uppercase text-center py-3 px-6 mb-0">
-                    {{ __('Login') }}
+@section('login&Register')
+    <div class="flex flex-wrap w-full">
+        <div class="flex flex-col w-full md:w-1/2">
+            <div class="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
+                <p class="text-3xl text-center text-gray-800">
+                    Bienvenido Reclutador.
+                </p>
+            <form class="flex flex-col pt-3 md:pt-8" method="POST" action="{{ route('login') }}" novalidate>
+                @csrf
+                <div class="flex flex-col pt-4">
+                    <div class="flex relative ">
+                        <span class=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z">
+                                </path>
+                            </svg>
+                        </span>
+                        <input type="email" name="email" value="{{ old('email') }}" class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Email"/>
+                    </div>
+                    @error('email')
+                        <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-
-                <form class="py-10 px-5" method="POST" action="{{ route('login') }}" novalidate>
-                    @csrf
-
-                    <div class="flex flex-wrap mb-6">
-                        <label for="email" class="block text-gray-700 text-sm mb-2">{{ __('E-Mail Address') }}</label>
-                        <input id="email" type="email" class="p-3 bg-gray-200 rounded form-input w-full @error('email') border-red-500 border @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                <div class="flex flex-col pt-4 mb-12">
+                    <div class="flex relative ">
+                        <span class=" inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                            <svg width="15" height="15" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z">
+                                </path>
+                            </svg>
+                        </span>
+                        <input type="password" name="password" value="{{ old('password') }}" class=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Contraseña"/>
                     </div>
-
-                    <div class="flex flex-wrap mb-6">
-                        <label for="password" class="block text-gray-700 text-sm mb-2">{{ __('Password') }}</label>
-                        <input id="password" type="password" class="p-3 bg-gray-200 rounded form-input w-full @error('password') border-red-500 border @enderror" name="password" autocomplete="current-password">
-
-                        @error('password')
-                            <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-
-                    <div class="flex flex-wrap mb-6">
-                        <input class="mr-2" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                        <label class="block text-gray-700 text-sm" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-
-                    <div class="flex flex-wrap">
-                        <button type="submit" class="bg-teal-500 w-full hover:bg-teal-700 text-gray-100 p-3 focus:outline-none focus:shadow-outline font-semibold">
-                            {{ __('Login') }}
-                        </button>
-
-                        @if (Route::has('password.request'))
-                            <a class="text-sm text-gray-500 mt-5 text-center w-full" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                    </div>
-                </form>
+                    @error('password')
+                        <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    @if (Route::has('password.request'))
+                        <a class="text-sm text-gray-500 mt-5 text-center w-full" href="{{ route('password.request') }}">
+                            ¿Ha olvidado su contraseña?
+                        </a>
+                    @endif
+                </div>
+                <button type="submit" class="w-full px-4 py-2 text-base font-semibold text-center text-white transform transition hover:scale-105 duration-300 ease-in-out bg-gray-700 shadow-md hover:bg-gray-800 focus:outline-none focus:ring-2">
+                    <span class="w-full">
+                        Iniciar sesión
+                    </span>
+                </button>
+            </form>
+            <div class="pt-12 pb-12 text-center">
+                    <p>
+                        ¿Es reclutador y aún no tiene una cuenta?
+                        <a href="{{ route('register') }}" class="font-semibold underline">
+                            Regístrese aquí.
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
+        <div class="w-1/2 shadow-2xl">
+            <img class="hidden object-cover w-full h-screen md:block" src="{{ asset('img/work.jpg') }}"/>
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,19 +22,14 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="leading-normal tracking-normal gradient bg-gray-200" style="font-family: 'Source Sans Pro', sans-serif;">
+<body class="leading-normal tracking-normal bg-gray-200 h-screen" style="font-family: 'Source Sans Pro', sans-serif;">
     <!--Nav-->
     <nav id="header" class="fixed w-full z-30 top-0 text-white">
       <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div class="pl-4 flex items-center">
           <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="{{ url('/') }}">
-            <!--Icon from: http://www.potlabicons.com/ -->
-            <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
-              <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
-              <path
-                class="plane-take-off"
-                d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-              />
+            <svg class="h-12 fill-current inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
             </svg>
             {{ config('app.name', 'Laravel') }}
           </a>
@@ -94,6 +89,8 @@
                 @yield('navigation')
             </nav>
         </div>
+
+        @yield('login&Register')
 
         <main class="mt-10 container mx-auto py-4">
             @yield('content')
