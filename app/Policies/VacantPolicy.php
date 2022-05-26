7267f2f3ae719b586d93536293700e843b704087
juanr10 <argudojuan7@gmail.com>
2022-05-26 16:55:24 +0200

    clean policy unused methods

--- app/Policies/VacantPolicy.php
+++ app/Policies/VacantPolicy.php
@@ -11,17 +11,6 @@ class VacantPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can view the model.
      *
      * @param  \App\User  $user
@@ -31,17 +20,6 @@ class VacantPolicy
     public function view(User $user, Vacant $vacant)
     {
         return $user->id === $vacant->user_id;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
     }
 
     /**
@@ -66,29 +44,5 @@ class VacantPolicy
     public function delete(User $user, Vacant $vacant)
     {
         return $user->id === $vacant->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Vacant  $vacant
-     * @return mixed
-     */
-    public function restore(User $user, Vacant $vacant)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Vacant  $vacant
-     * @return mixed
-     */
-    public function forceDelete(User $user, Vacant $vacant)
-    {
-        //
     }
 }
